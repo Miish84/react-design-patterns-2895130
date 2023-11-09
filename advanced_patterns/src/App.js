8e435cd28302cf4adfe334404b6ec18f6ebe9d56
MichaelIshee <mishee@crsdata.com>
2023-11-09 15:34:28 -0500

    splitscreen

---
+++ advanced_patterns/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { SplitScreen } from './SplitScreen';
+
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>
+}
+
+const RightHandComponent = ({ message }) => {
+  return <h1 style={{ backgroundColor: 'purple' }}>{message}</h1>
+}
+
+function App() {
+  return (
+    <SplitScreen
+      leftWeight={1}
+      rightWeight={3} >
+      <LeftHandComponent name="Michael" />
+      <RightHandComponent message="Hello" />
+    </SplitScreen>
+  );
+}
+
+export default App;
